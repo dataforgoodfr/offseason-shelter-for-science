0c71d2b104e864df9d7e4ea6e93fd846b04f2c39
khillion <kenzo-hugo.hillion.ext@allianz-trade.com>
2025-06-03 22:00:39 +0200

    feat: add offset for list records

--- nocodb/tables.py
+++ nocodb/tables.py
@@ -1,5 +1,5 @@
 # coding: utf-8
 
-TABLE_DG_DATASETS = 'datagov_datasets'
-TABLE_DG_ORGANIZATIONS = 'datagov_organizations'
-TABLE_DG_RESOURCES = 'datagov_resources'
+TABLE_DG_DATASETS = "datagov_datasets"
+TABLE_DG_ORGANIZATIONS = "datagov_organizations"
+TABLE_DG_RESOURCES = "datagov_resources"
