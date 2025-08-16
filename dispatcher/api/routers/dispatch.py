b162d1219136c527b57890e8f71cf41161c3f510
cp19 <b00698082@essec.edu>
2025-08-16 13:51:11 +0200

    ADD dispatcher logic from a flat ranker file

--- dispatcher/api/routers/dispatch.py
+++ dispatcher/api/routers/dispatch.py
@@ -1,3 +1,4 @@
+from models.state import app_state
 from fastapi import HTTPException, APIRouter
 from fastapi.responses import JSONResponse
 from models.payload import DispatchRequest, DispatchResponse
@@ -12,18 +13,44 @@ async def root():
     return JSONResponse(content=data)
 
 @router.post('/mock_dispatch', response_model=DispatchResponse)
-async def mock_dispatch(payload: DispatchRequest):
+async def mock_dispatch(request: DispatchRequest):
+    app_state._logger.info("________In mock dispatch")
     # Load mock data from the JSON file
-    print(f"mock_dispatch payload: {payload}")
+    print(f"mock_dispatch payload: {request}")
     with open(join(dirname(__file__), '..', 'mock', 'mock_data.json')) as f:
         mock_dispatch = json.load(f)
 
     distpatch_response = DispatchResponse(
         status="success",
-        message="Payload received and processed",
-        received_data=payload.dict(),
-        mock_data=mock_dispatch
+        message="Request received and processed",
+        received_data=request.dict(),
+        asset=mock_dispatch
     )
     print(f"mock_dispatch response: {distpatch_response}")
+
+    return distpatch_response
+
+@router.post('/dispatch', response_model=DispatchResponse)
+async def dispatch(request: DispatchRequest):
+    app_state._logger.info("________In dispatch")
+    # Call dispatcher logic
+    result = app_state._dispatcher.allocate_assets(
+        free_space_mb=request.free_space_gb * 1024,
+        node_id=request.node_id
+    )
+    app_state._logger.info(result)
+    if not result:
+        raise HTTPException(
+            status_code=404,
+            detail="No available assets matching the criteria"
+        )
+
+    distpatch_response = DispatchResponse(
+        status="success",
+        message="Request received and processed",
+        received_data=request.dict(),
+        asset=result["assets"]
+    )
+    print(f"Dispatch response: {distpatch_response}")
 
     return distpatch_response
--- test/dispatcher/test_dispatcher.py
+++ test/dispatcher/test_dispatcher.py
@@ -1,10 +1,24 @@
 import requests
 
-url = "http://127.0.0.1:8000/mock_dispatch"
-payload = {
+url_mock = "http://127.0.0.1:8001/mock_dispatch"
+payload_mock = {
     "name": "mock_dispatch_request", 
     "description": "This is a test dispatch", 
-    "value": 10737418240
+    "free_space_gb": 10
+    }
+print(payload_mock)
+response_mock = requests.post(url_mock, json=payload_mock)
+print(response_mock.headers)
+print(response_mock.status_code)
+print(response_mock.json())
+print("__________________________________________________________________")
+
+url = "http://127.0.0.1:8001/dispatch"
+payload = {
+    "name": "test_dispatch_request", 
+    "description": "This is a test dispatch", 
+    "free_space_gb": 10,
+    "node_id": "1"
     }
 print(payload)
 response = requests.post(url, json=payload)
